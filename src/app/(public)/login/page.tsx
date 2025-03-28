5aab0af10051a946cbd8c17c342ed47f0bd2c98c
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-03-28 20:04:23 -0300

    feat: create Login page

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,7 +1,131 @@
-export default function SignIn() {
+"use client";
+
+import React from "react";
+import { Button, Input, Checkbox, Link, Form, Divider } from "@heroui/react";
+import { Icon } from "@iconify/react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const [isVisible, setIsVisible] = React.useState<boolean>(false);
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      console.log(result);
+
+      return;
+    }
+
+    router.replace("/");
+  };
+
   return (
-    <main>
-      <h1>SignIn</h1>
-    </main>
+    <div className="flex h-screen w-screen items-center justify-center scale-125">
+      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 pb-10 pt-6 shadow-small">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-large font-medium">Sign in to your account</h1>
+        </div>
+
+        <Form
+          className="flex flex-col gap-3"
+          validationBehavior="native"
+          onSubmit={handleSubmit}
+        >
+          <Input
+            isRequired
+            classNames={{
+              input:
+                "text-small focus:outline-none border-transparent focus:border-transparent focus:ring-0 p-0",
+            }}
+            label="Email Address"
+            name="email"
+            placeholder="Enter your email"
+            type="email"
+            variant="bordered"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            isRequired
+            classNames={{
+              input:
+                "text-small focus:outline-none border-transparent focus:border-transparent focus:ring-0 p-0",
+            }}
+            endContent={
+              <button type="button" onClick={toggleVisibility}>
+                {isVisible ? (
+                  <Icon
+                    className="pointer-events-none text-2xl text-default-400"
+                    icon="solar:eye-closed-linear"
+                  />
+                ) : (
+                  <Icon
+                    className="pointer-events-none text-2xl text-default-400"
+                    icon="solar:eye-bold"
+                  />
+                )}
+              </button>
+            }
+            label="Password"
+            name="password"
+            placeholder="Enter your password"
+            type={isVisible ? "text" : "password"}
+            variant="bordered"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="flex w-full items-center justify-between px-1 py-2">
+            <Checkbox name="remember" size="sm">
+              Remember me
+            </Checkbox>
+            <Link className="text-default-500" href="#" size="sm">
+              Forgot password?
+            </Link>
+          </div>
+          <Button className="w-full" color="primary" type="submit">
+            Sign In
+          </Button>
+        </Form>
+        <div className="flex items-center gap-4 py-2">
+          <Divider className="flex-1" />
+          <p className="shrink-0 text-tiny text-default-500">OR</p>
+          <Divider className="flex-1" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Button
+            startContent={<Icon icon="flat-color-icons:google" width={24} />}
+            variant="bordered"
+          >
+            Continue with Google
+          </Button>
+          <Button
+            startContent={
+              <Icon className="text-default-500" icon="fe:github" width={24} />
+            }
+            variant="bordered"
+          >
+            Continue with Github
+          </Button>
+        </div>
+        <p className="text-center text-small">
+          Need to create an account?&nbsp;
+          <Link className="font-semibold" href="/register" size="sm">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
   );
 }
