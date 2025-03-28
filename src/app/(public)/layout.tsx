29f796047ae5735f085a7194a8c590a91a696af5
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-03-28 20:13:40 -0300

    refactor: improves PublicLayout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,9 @@
 import "@/src/styles/globals.css";
 import { Metadata, Viewport } from "next";
+import clsx from "clsx";
 
 import { siteConfig } from "@/src/config/site";
+import { fontSans } from "@/src/config/fonts";
 
 export const metadata: Metadata = {
   title: {
@@ -27,9 +29,15 @@ export default function PublicLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <h1>Public</h1>
-      {children}
+    <div
+      className={clsx(
+        "min-h-screen bg-background font-sans antialiased dark:bg-gray-950 w-full",
+        fontSans.variable,
+      )}
+    >
+      <div className="h-screen flex">
+        <div className=" bg-slate-100">{children}</div>
+      </div>
     </div>
   );
 }
