556b0c79513fdf2e4a1fb78d4ecbe32b3551ae13
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-11 16:28:27 -0300

    refactor: remove github signin button

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -110,14 +110,6 @@ export default function LoginPage() {
           >
             Continue with Google
           </Button>
-          <Button
-            startContent={
-              <Icon className="text-default-500" icon="fe:github" width={24} />
-            }
-            variant="bordered"
-          >
-            Continue with Github
-          </Button>
         </div>
         <p className="text-center text-small">
           Need to create an account?&nbsp;
