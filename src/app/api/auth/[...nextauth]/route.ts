06245931a0487f2b0c11a575fa6c5c686b013523
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-11 16:31:49 -0300

    feat: add login logic in authorize method of NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,30 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { compare } from "bcrypt-ts";
+
+import { getUser } from "@/src/lib/db";
 
 const nextAuthOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: { label: "email", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+      async authorize({ email, password }: any) {
+        const user = await getUser(email);
 
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
+        if (!user.length) return null;
+        let passwordsMatch = await compare(password, user[0].password!);
 
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
+        if (passwordsMatch) return user[0] as any;
       },
     }),
   ],
   pages: {
     signIn: "/login",
+    newUser: "/register",
   },
 };
 
