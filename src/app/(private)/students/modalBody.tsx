9c058309b791bd327f23814d7c4616bde3a50f14
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-16 20:58:19 -0300

    feat: add avatar Input and status Select in ModalBodyCreate

--- src/app/(private)/students/modalBody.tsx
+++ src/app/(private)/students/modalBody.tsx
@@ -5,7 +5,7 @@ import React, {
   useRef,
 } from "react";
 import { Chip, ChipProps, Input, Select, SelectItem } from "@heroui/react";
-import { IdCard, User as UserIcon } from "lucide-react";
+import { CircleUser, IdCard, User as UserIcon } from "lucide-react";
 
 import { MailIcon } from "@/src/config/icons";
 
@@ -18,20 +18,43 @@ export type CreateFormRef = {
 };
 
 export const ModalBodyCreate = forwardRef<CreateFormRef>((_, ref) => {
+  const avatarInputRef = useRef<HTMLInputElement>(null);
   const nameInputRef = useRef<HTMLInputElement>(null);
   const emailInputRef = useRef<HTMLInputElement>(null);
   const cpfInputRef = useRef<HTMLInputElement>(null);
+  const statusValueRef = useRef<HTMLSelectElement>(null);
 
   useImperativeHandle(ref, () => ({
     getFormData: () => ({
+      avatar: avatarInputRef.current?.value || "",
       name: nameInputRef.current?.value || "",
       email: emailInputRef.current?.value || "",
       cpf: cpfInputRef.current?.value || "",
+      status: statusValueRef.current?.value || "",
     }),
   }));
 
   return (
     <div className="space-y-4">
+      <Input
+        ref={avatarInputRef}
+        classNames={{
+          input:
+            "text-small focus:outline-none border-transparent focus:border-transparent focus:ring-0 p-0",
+        }}
+        endContent={
+          <CircleUser className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+        }
+        label="Avatar"
+        placeholder="Enter your avatar url"
+        startContent={
+          <div className="pointer-events-none flex items-center">
+            <span className="text-default-400 text-small">https://</span>
+          </div>
+        }
+        type="url"
+        variant="bordered"
+      />
       <Input
         ref={nameInputRef}
         classNames={{
@@ -71,6 +94,47 @@ export const ModalBodyCreate = forwardRef<CreateFormRef>((_, ref) => {
         placeholder="Enter your cpf"
         variant="bordered"
       />
+      <Select
+        ref={statusValueRef}
+        classNames={{
+          label: "group-data-[filled=true]:-translate-y-3.5",
+          trigger: "max-w-[10rem]",
+          listboxWrapper: "max-h-[400px]",
+        }}
+        items={status}
+        label="Status"
+        placeholder="Select an status"
+        renderValue={(items) => {
+          return items.map((item) => (
+            <div key={item.key} className="flex items-center gap-2">
+              <Chip
+                className="capitalize"
+                color={statusColorMap[item.data!.status]}
+                size="sm"
+                variant="flat"
+              >
+                {item.data!.status}
+              </Chip>
+            </div>
+          ));
+        }}
+        variant="bordered"
+      >
+        {(status) => (
+          <SelectItem key={status.status} textValue={status.status}>
+            <div className="flex gap-2 items-center">
+              <Chip
+                className="capitalize"
+                color={statusColorMap[status.status]}
+                size="sm"
+                variant="flat"
+              >
+                {status.status}
+              </Chip>
+            </div>
+          </SelectItem>
+        )}
+      </Select>
     </div>
   );
 });
