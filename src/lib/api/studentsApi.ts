e9d7662d97f3ddb41720dbed567e88ad04fdad17
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-22 22:10:24 -0300

    refactor: add api url on studentsApi

--- src/lib/api/studentsApi.ts
+++ src/lib/api/studentsApi.ts
@@ -1,9 +1,9 @@
 import { User as IUser } from "@/src/types";
 
+const url = "https://student-hub-api.onrender.com/v1/";
+
 export async function getStudents(): Promise<IUser[] | []> {
-  const studentsResponse = await fetch(
-    "http://localhost:3001/v1/list/students",
-  );
+  const studentsResponse = await fetch(`${url}list/students`);
 
   if (!studentsResponse.ok) {
     throw new Error("Erro ao buscar dados da API");
@@ -15,7 +15,7 @@ export async function getStudents(): Promise<IUser[] | []> {
 export async function createStudent(
   student: Omit<IUser, "id" | "ra">,
 ): Promise<void> {
-  const response = await fetch("http://localhost:3001/v1/create/student", {
+  const response = await fetch(`${url}create/student`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -32,7 +32,7 @@ export async function createStudent(
 export async function updateStudent(
   student: Omit<IUser, "id" | "ra">,
 ): Promise<void> {
-  const response = await fetch("http://localhost:3001/v1/update/student", {
+  const response = await fetch(`${url}update/student`, {
     method: "PATCH",
     headers: {
       Accept: "application/json",
@@ -49,12 +49,9 @@ export async function updateStudent(
 export async function deleteStudent({
   cpf,
 }: Pick<IUser, "cpf">): Promise<void> {
-  const response = await fetch(
-    `http://localhost:3001/v1/delete/student?cpf=${cpf}`,
-    {
-      method: "DELETE",
-    },
-  );
+  const response = await fetch(`${url}delete/student?cpf=${cpf}`, {
+    method: "DELETE",
+  });
 
   if (!response.ok) {
     throw new Error("Erro ao buscar dados da API");
