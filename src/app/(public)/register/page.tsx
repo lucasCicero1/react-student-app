9215b4897ffdc824ac91f526dc95a17608be5654
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-28 21:18:05 -0300

    refactor: improve Login and Register page responsiveness

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -51,7 +51,7 @@ export default function RegisterPage() {
   };
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="scale-90 sm:scale-100 flex h-screen w-screen items-center justify-center">
       <div className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 pb-10 pt-6 shadow-small">
         <div className="flex flex-col gap-1">
           <h1 className="text-large font-medium">Sign Up</h1>
