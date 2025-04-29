ae86be3b9cf712faf9f0d41a7aceb187a9a2f216
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-04-28 21:11:23 -0300

    refactor: improve PrivateLayout responsiveness

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -55,15 +55,16 @@ export default async function PrivateLayout({
         <ReactQueryProvider>
           <div className="h-screen flex">
             {/* LEFT */}
-            <div className="w-[14%] md:w-[10%] lg:w-[16%] xl:w-[14%]">
-              <div className="h-[60px] flex items-center justify-between px-8 bg-slate-700 dark:bg-slate-950">
+            {/* w-[14%] */}
+            <div className="w-[0%] sm:w-[8%] md:w-[10%] lg:w-[16%] xl:w-[14%]">
+              <div className="h-[60px] sm:px-2 sm:text-xs md:px-4 md:text-sm flex items-center justify-between px-8 bg-slate-700 dark:bg-slate-950">
                 <Link href="/">
-                  <span className="hidden lg:block text-gray-300 font-medium">
+                  <span className="hidden sm:block lg:block text-gray-300 font-medium">
                     Achoo
                   </span>
                 </Link>
                 <IconMenu
-                  className="text-slate-500 cursor-pointer"
+                  className="sm:hidden lg:block text-slate-500 cursor-pointer"
                   height={25}
                   width={25}
                 />
@@ -72,9 +73,12 @@ export default async function PrivateLayout({
             </div>
 
             {/* RIGHT */}
-            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-slate-100 dark:bg-slate-800 dark:border-l-1 border-transparent dark:border-default-300 overflow-y-scroll">
+            {/* w-[86%] */}
+            <div className="w-[100%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-slate-100 dark:bg-slate-800 dark:border-l-1 border-transparent dark:border-default-300 overflow-y-scroll">
               <Navbar user={session?.user?.name} />
-              <div className="flex-1 mx-20">{children}</div>
+              <div className="flex-1 mx-6 sm:mx-12 md:mx-16 lg:mx-20 xl:mx-24">
+                {children}
+              </div>
             </div>
           </div>
         </ReactQueryProvider>
