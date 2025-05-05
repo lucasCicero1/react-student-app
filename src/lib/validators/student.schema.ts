f00b53d898759d0a6c5a904c0975efebc810a65b
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-05-05 20:18:50 -0300

    feat: create studentSchema zod validation

---
+++ src/lib/validators/student.schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const studentSchema = z.object({
+  avatar: z.optional(z.string()),
+  name: z.string().min(3, "Name is required"),
+  email: z.string().email("Invalid email address"),
+  cpf: z.string().length(11, "Cpf must have 11 characters"),
+  status: z.string(),
+});
+
+export type StudentFormData = z.infer<typeof studentSchema>;
