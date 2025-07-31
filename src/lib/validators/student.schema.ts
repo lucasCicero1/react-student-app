c5e39884824cdbb4403a12e4adf125eb0e4a2d8d
Lucas Cícero <lucas.cicerofs@outlook.com>
2025-07-30 22:01:15 -0300

    chore: update studentSchema email zod validation

--- src/lib/validators/student.schema.ts
+++ src/lib/validators/student.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const studentSchema = z.object({
   avatar: z.optional(z.string()),
   name: z.string().min(3, "Name is required"),
-  email: z.string().email("Invalid email address"),
+  email: z.email("Invalid email address"),
   cpf: z.string().length(11, "Cpf must have 11 characters"),
   status: z.string(),
 });
